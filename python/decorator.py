8204b09c47bb1a6320a17f1e92206053d8202c95
2393494020 <house.deng@qq.com>
2020-03-30 12:41:28 +0800

    python

---
+++ python/decorator.py
@@ -0,0 +1,36 @@
+import time
+
+
+# 装饰器
+def deco(func):
+    def wrapper(*args):
+        begin = time.time()
+        result = func(*args)
+        end = time.time()
+        print('Total time: {:.3} s'.format( end - begin ))
+        return result
+
+    return wrapper
+
+def is_prime(num):
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+
+@deco
+def count_prime_num(maxnum):
+    count = 0
+    for num in range(2, maxnum):
+        if is_prime(num):
+            count = count + 1
+    
+    return(count)
+
+if __name__ == '__main__':
+    print(count_prime_num(10000))
